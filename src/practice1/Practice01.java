82ae5ecaad830830f18950a1d598a39e449186d4
coffeeful <dltjdgus03@gmail.com>
2023-11-30 10:35:25 +0900

    practice commit

---
+++ src/practice1/Practice01.java
@@ -0,0 +1,12 @@
+package practice1;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("우리 3조 화이팅!");
+		
+
+	}
+
+}

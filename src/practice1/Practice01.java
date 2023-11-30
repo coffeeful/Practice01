badb23bb41da050a305bf2afd59d16209eec59e5
coffeeful <dltjdgus03@gmail.com>
2023-11-30 10:38:05 +0900

    날씨가 추움

--- src/practice1/Practice01.java
+++ src/practice1/Practice01.java
@@ -4,7 +4,7 @@ public class Practice01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("우리 3조 화이팅!");
+		System.out.println("날씨가 많이 춥네요");
 		
 
 	}
